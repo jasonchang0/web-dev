bca99503767898a3b2fee00e4b4d10349df3b269
Jason Chang <jasonchang0@berkeley.edu>
2019-01-10 12:02:01 -0800

    defining class in class syntax and function syntax

--- scripts/JavaScript/script.js
+++ scripts/JavaScript/script.js
@@ -260,6 +260,13 @@ class car {
     }
 }
 
+// Define class as only the constructor function
+// function car(type, color, miles) {
+//     this.type = type;
+//     this.color = color;
+//     this.miles = miles;
+// }
+
 var car1 = new car("Compact", "blue", 6580);
 var car2 = new car("Truck", "red", 397);
 var car3 = new car("SUV", "yellow", 9948);
